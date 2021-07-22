f27c4bcbe6c883a755e70299817eb9e68a70679e
AJ Auntor <auntorknocks@gmail.com>
2021-07-22 22:21:42 +0600

    Header Right added

--- Header.js
+++ Header.js
@@ -3,8 +3,11 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="header">
+
             <div className="header_left"></div>
             <div className="header_middle"></div>
+            <div className="header_right"></div>
+
         </div>
     );
 };
