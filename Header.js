1dd31e6823ee65dc0a35b4290443fa469779c7be
AJ Auntor <auntorknocks@gmail.com>
2021-07-22 22:18:29 +0600

    Remove H1 tag

--- Header.js
+++ Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="header">
-            <h1>This is Header Component!</h1>
+            
         </div>
     );
 };
