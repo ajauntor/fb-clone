de64acabd0b70d3074a513d65cba640b503dfe60
AJ Auntor <auntorknocks@gmail.com>
2021-07-22 22:52:31 +0600

    logo added

--- Header.js
+++ Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import logo from './src/image/logo.png'
 
 const Header = () => {
     return (
         <div className="header">
 
             <div className="header_left">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/600px-Facebook_Logo_%282019%29.png" alt="" />
+                <img src={logo} alt="" />
             </div>
 
             <div className="header_middle"></div>
-            
+
             <div className="header_right"></div>
 
         </div>
