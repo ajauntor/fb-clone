913b97c51e5351ec000ee5080fa04db9a53c7b93
AJ Auntor <auntorknocks@gmail.com>
2021-07-22 22:40:39 +0600

    added logo

--- Header.js
+++ Header.js
@@ -4,8 +4,12 @@ const Header = () => {
     return (
         <div className="header">
 
-            <div className="header_left"></div>
+            <div className="header_left">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/600px-Facebook_Logo_%282019%29.png" alt="" />
+            </div>
+
             <div className="header_middle"></div>
+            
             <div className="header_right"></div>
 
         </div>
