4ca8d4cdef7bed65296675b0cfd1c208f7b5398a
AJ Auntor <auntorknocks@gmail.com>
2021-07-23 14:32:54 +0600

    Tested the css

--- src/Component/SideBar.js
+++ src/Component/SideBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './SideBar.css'
 
 const SideBar = () => {
     return (
-        <div>
-            
+        <div className="sidebar">
+            <h1>This is boom</h1>
         </div>
     );
 };
