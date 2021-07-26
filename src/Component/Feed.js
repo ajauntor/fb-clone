b85a5c6d6b601c8beb48d885e837369c6363b492
AJ Auntor <auntorknocks@gmail.com>
2021-07-26 22:05:37 +0600

    storyReel import

--- src/Component/Feed.js
+++ src/Component/Feed.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Feed.css'
+import StoryReel from './StoryReel';
 
 const Feed = () => {
     return (
         <div className="feed">
-            {/* StoryReel */}
+            <StoryReel />
             {/* MessageSender */}
         </div>
     );
--- src/Component/StoryReel.js
+++ src/Component/StoryReel.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './StoryReel.css'
 
 const StoryReel = () => {
     return (
