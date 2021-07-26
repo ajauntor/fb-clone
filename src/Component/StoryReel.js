425279ae4e13a2838327691713220a74b819614a
AJ Auntor <auntorknocks@gmail.com>
2021-07-26 22:34:43 +0600

    story final

--- src/Component/StoryReel.js
+++ src/Component/StoryReel.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import Story from './Story';
 import './StoryReel.css'
 
 const StoryReel = () => {
     return (
         <div className="storyReel">
-            {/* story */}
-            {/* story */}
-            {/* story */}
-            {/* story */}
-            {/* story */}
+            <Story image="https://cdn.britannica.com/45/223045-050-A6453D5D/Telsa-CEO-Elon-Musk-2014.jpg"
+                    profileSrc="https://static.dezeen.com/uploads/2021/06/elon-musk-architect_dezeen_1704_col_0.jpg"
+                    title="Elon Musk"
+                    />
+
+            <Story image="https://cdn.britannica.com/45/223045-050-A6453D5D/Telsa-CEO-Elon-Musk-2014.jpg"
+                    profileSrc="https://static.dezeen.com/uploads/2021/06/elon-musk-architect_dezeen_1704_col_0.jpg"
+                    title="Elon Musk"
+                    />
+
+            <Story image="https://cdn.britannica.com/45/223045-050-A6453D5D/Telsa-CEO-Elon-Musk-2014.jpg"
+                    profileSrc="https://static.dezeen.com/uploads/2021/06/elon-musk-architect_dezeen_1704_col_0.jpg"
+                    title="Elon Musk"
+                    />
+
+            <Story image="https://cdn.britannica.com/45/223045-050-A6453D5D/Telsa-CEO-Elon-Musk-2014.jpg"
+                    profileSrc="https://static.dezeen.com/uploads/2021/06/elon-musk-architect_dezeen_1704_col_0.jpg"
+                    title="Elon Musk"
+                    />
+
+            <Story image="https://cdn.britannica.com/45/223045-050-A6453D5D/Telsa-CEO-Elon-Musk-2014.jpg"
+                    profileSrc="https://static.dezeen.com/uploads/2021/06/elon-musk-architect_dezeen_1704_col_0.jpg"
+                    title="Elon Musk"
+                    />
         </div>
     );
 };
