54809822dc3d35b5a7303bf5ee84e3bd0d6cdef4
AJ Auntor <auntorknocks@gmail.com>
2021-07-26 22:26:35 +0600

    Avatar added

--- src/Component/Story.js
+++ src/Component/Story.js
@@ -1,10 +1,11 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react';
 import './Story.css'
 
 const Story = ({image, profileSrc, title }) => {
     return (
         <div className="story">
-            
+            <Avatar src={profileSrc} />
         </div>
     );
 };
