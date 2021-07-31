3437dd3aebd15e81bc42293f518d79195508822b
AJ Auntor <auntorknocks@gmail.com>
2021-07-31 22:00:53 +0600

    post_topInfo

--- src/Component/Post.js
+++ src/Component/Post.js
@@ -1,9 +1,14 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react';
 import './Post.css'
 
 const Post = ({profilePic, image, userName, timeStamp, message}) => {
     return (
         <div className="post">
+
+            <div className="post_top">
+                <Avatar src ={profilePic} className="post_topInfo" />
+            </div>
             
         </div>
     );
