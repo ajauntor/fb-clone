2fc8c54f7fd4988e8621aa89e7d692e1821028d6
AJ Auntor <auntorknocks@gmail.com>
2021-07-31 21:44:12 +0600

    Post Component on feed page linkUp

--- src/Component/Feed.js
+++ src/Component/Feed.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Feed.css'
 import MessageSender from './MessageSender';
+import Post from './Post';
 import StoryReel from './StoryReel';
 
 const Feed = () => {
     return (
         <div className="feed">
+
             <StoryReel />
+
             <MessageSender />
+
+            <Post />
             
         </div>
     );
