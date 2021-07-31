21d8309ba7f1ec7e444ba46a806e85ce4fa5e6db
AJ Auntor <auntorknocks@gmail.com>
2021-07-31 22:26:05 +0600

    fakeData Imported

--- src/Component/Feed.js
+++ src/Component/Feed.js
@@ -12,9 +12,29 @@ const Feed = () => {
 
             <MessageSender />
 
-            <Post />
-            <Post />
-            <Post />
+            <Post
+            profilePic="https://www.tbsnews.net/sites/default/files/styles/infograph/public/images/2021/07/08/img_2776.jpg?itok=v424ATY4"
+            message="I am gonna moonland today"
+            timeStamp="This is Time Stamp"
+            userName="Jhankar Mahbub"
+            image="https://i.ytimg.com/vi/TSVjANr7I5o/maxresdefault.jpg"
+            />
+            
+            <Post
+            profilePic="https://www.tbsnews.net/sites/default/files/styles/infograph/public/images/2021/07/08/img_2776.jpg?itok=v424ATY4"
+            message="I am gonna moonland today"
+            timeStamp="This is Time Stamp"
+            userName="Jhankar Mahbub"
+            image="https://i.ytimg.com/vi/TSVjANr7I5o/maxresdefault.jpg"
+            />
+
+            <Post
+            profilePic="https://www.tbsnews.net/sites/default/files/styles/infograph/public/images/2021/07/08/img_2776.jpg?itok=v424ATY4"
+            message="I am gonna moonland today"
+            timeStamp="This is Time Stamp"
+            userName="Jhankar Mahbub"
+            image="https://i.ytimg.com/vi/TSVjANr7I5o/maxresdefault.jpg"
+            />
 
         </div>
     );
