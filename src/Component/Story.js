2a524c790c66a0ac8b0e76e706532ecc36a63ce5
AJ Auntor <auntorknocks@gmail.com>
2021-08-16 22:55:32 +0600

    .

--- src/Component/Story.js
+++ src/Component/Story.js
@@ -2,12 +2,7 @@ import { Avatar } from '@material-ui/core';
 import React from 'react';
 import './Story.css'
 
-const Story = ({image, profileSrc, title }) => {
-    return (
-        <div style={{backgroundImage: `url(${image})`}} className="story">
-            <Avatar style={{border:"5px solid #2e81f4", margin:"10px"}} src={profileSrc} />
-            <h4>{title}</h4>
-        </div>
+
     );
 };
 
