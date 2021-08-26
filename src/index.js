6609b74b4aaf94d75771d45c043fa6c8183f28b5
AJ Auntor <auntorknocks@gmail.com>
2021-08-26 18:28:24 +0600

    error popup

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StateProvider } from './Component/StateProvider';
+import reducer, { initialState } from './Component/reducer';
 
 ReactDOM.render(
   <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
     <App />
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
