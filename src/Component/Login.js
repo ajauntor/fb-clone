ff0cb98bf3b0df92a2c64ed060b12b614e50312d
AJ Auntor <auntorknocks@gmail.com>
2021-08-26 17:46:39 +0600

    auth redirect to realtime database

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,12 +1,18 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
 import './Login.css'
+import { auth, provider } from './firebase.config';
 
 const Login = () => {
 
     const signIn = () =>{
         //Sign In....
-    }
+        auth.signInWithPopup(provider)
+        .then(result => {
+            console.log(result)
+        })
+        .catch((error) => alert(error.message));
+    };
 
     return (
         <div className="login">
--- src/Component/firebase.config.js
+++ src/Component/firebase.config.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyALPOsm4VTGn2BbhKMOvlx_aR-xwzZVUrU",
     authDomain: "facebook-clone-8d075.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
     messagingSenderId: "422253664407",
     appId: "1:422253664407:web:fd8018ee20a8014efef1d2"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export {auth, provider};
+  export default db;
