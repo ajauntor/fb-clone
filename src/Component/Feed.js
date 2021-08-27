6ef3233e84ecdb0c58ea5321e58acc5ac5311af5
AJ Auntor <auntorknocks@gmail.com>
2021-08-27 14:31:51 +0600

    Data load from database

--- src/Component/Feed.js
+++ src/Component/Feed.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './Feed.css'
+import db from './firebase.config';
 import MessageSender from './MessageSender';
 import Post from './Post';
 import StoryReel from './StoryReel';
 
 const Feed = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => { 
+        db.collection("posts").onSnapshot((snapshot) =>
+            setPosts(snapshot.docs.map(doc => ({id: doc.id, data: doc.data() })))
+        );
+    }, []);
+
+
     return (
         <div className="feed">
 
@@ -12,30 +24,16 @@ const Feed = () => {
 
             <MessageSender />
 
+        {posts.map((post) => (
             <Post
-            profilePic="https://www.tbsnews.net/sites/default/files/styles/infograph/public/images/2021/07/08/img_2776.jpg?itok=v424ATY4"
-            message="I am gonna moonland today"
-            timeStamp="This is Time Stamp"
-            userName="Jhankar Mahbub"
-            image="https://i.ytimg.com/vi/TSVjANr7I5o/maxresdefault.jpg"
+            key={posts.id}
+            profilePic={post.profilePic}
+            message={post.message}
+            timeStamp={post.timestamp}
+            userName={post.username}
+            image={post.image}
             />
-            
-            <Post
-            profilePic="https://www.tbsnews.net/sites/default/files/styles/infograph/public/images/2021/07/08/img_2776.jpg?itok=v424ATY4"
-            message="I am gonna moonland today"
-            timeStamp="This is Time Stamp"
-            userName="Jhankar Mahbub"
-            image="https://i.ytimg.com/vi/TSVjANr7I5o/maxresdefault.jpg"
-            />
-
-            <Post
-            profilePic="https://www.tbsnews.net/sites/default/files/styles/infograph/public/images/2021/07/08/img_2776.jpg?itok=v424ATY4"
-            message="I am gonna moonland today"
-            timeStamp="This is Time Stamp"
-            userName="Jhankar Mahbub"
-            image="https://i.ytimg.com/vi/TSVjANr7I5o/maxresdefault.jpg"
-            />
-
+        ))}
         </div>
     );
 };
