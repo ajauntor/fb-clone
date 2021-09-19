6e247ec772b91eb7ac7e0eb67bfbee4fb2831145
AJ Auntor <auntorknocks@gmail.com>
2021-09-19 22:53:12 +0600

    .

--- src/Component/reducer.js
+++ src/Component/reducer.js
@@ -10,7 +10,8 @@ const reducer = (state, action) => {
     switch(action.type) {
         case actionTypes.SET_USER:
             return {
-               
+                ...state,
+                user: action.user,
             };
 
             default:
