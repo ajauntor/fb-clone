777dfac7b52e2306a782f0e9aea2908bdf9aded6
AJ Auntor <auntorknocks@gmail.com>
2021-10-20 23:31:37 +0600

    .

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StateProvider } from './Component/StateProvider';
-import reducer, { initialState } from './Component/reducer';
+
 
 ReactDOM.render(
   <React.StrictMode>
