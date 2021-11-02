58e32c1bf28555ab08743bfa4c847d09bd146a6d
AJ Auntor <auntorknocks@gmail.com>
2021-11-02 23:49:54 +0600

    .

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -4,7 +4,7 @@ const reportWebVitals = onPerfEntry => {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
-      
+      getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
     });
